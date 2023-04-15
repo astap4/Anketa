f2d8e8ba7f192fc32a491d490dfc98b7fce5bc6d
Alina <astapchik.1993@mail.ru>
2023-04-15 22:08:25 +0300

    fix: task removeWhitespaces

--- A2+/index.js
+++ A2+/index.js
@@ -1,31 +1,27 @@
-while (true) {
-    yourString = prompt('Введите строку');
-    if (yourString) {
-        break
-    } else {
-        alert("Строка не может быть пустая!");
-    }
-}
+yourString = prompt('Введите строку');
+
 alert(`%${removeWhitespaces(yourString)}%`)
 
 function removeWhitespaces(string) {
     let i = 0;
     let j = string.length - 1;
+    if (i != ' ' && j != ' ') { //Если нет пробела ни в начале, нив  конце - позвращаем исходную строку
+        return string;
+    }
     if (string[0] === ' ') {    //Если нет пробела в начале - пропускаем цикл
         while (string[i] === " ") {
             i++;
         }
-    }
-    if (i === string.length) {   //Если строка состоит только из пробелов - возвращаем пустую строку после первого цикла
-        return '';
-    } else {
-        if (string[j] === ' ') {   //Если нет пробела в конце - пропускаем цикл
-            while (string[j] === " ") {
-                j--;
-            }
+        if (i === string.length) {
+            return '';                //Если строка состоит только из пробелов - возвращаем пустую строку после первого цикла
         }
-        return string.slice(i, j + 1)
     }
+    if (string[j] === ' ') {   //Если нет пробела в конце - пропускаем цикл
+        while (string[j] === " ") {
+            j--;
+        }    
+    }
+    return string.slice(i, j + 1)
 }
 
 
